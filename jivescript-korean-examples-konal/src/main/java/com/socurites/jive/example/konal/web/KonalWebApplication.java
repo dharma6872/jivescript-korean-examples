850c3a274e8c4917e14a718c552c30acadb8917a
naaras.malsami <naras.malsami@gmail.com>
2016-09-28 18:36:28 +0900

    add SpringBootApplication annotation

--- jivescript-korean-examples-konal/src/main/java/com/socurites/jive/example/konal/web/KonalWebApplication.java
+++ jivescript-korean-examples-konal/src/main/java/com/socurites/jive/example/konal/web/KonalWebApplication.java
@@ -2,12 +2,14 @@ package com.socurites.jive.example.konal.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+@SpringBootApplication
 public class KonalWebApplication {
 	public static void main(String[] args) {
         SpringApplication.run(KonalWebApplication.class, args);
