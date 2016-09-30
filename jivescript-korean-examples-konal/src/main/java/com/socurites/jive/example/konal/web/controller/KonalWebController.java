3a5aef8769d62157d50e34a4a82128e1a1ec60c8
naaras.malsami <naras.malsami@gmail.com>
2016-09-30 12:07:11 +0900

    add district repository as a delegate for db

--- jivescript-korean-examples-konal/src/main/java/com/socurites/jive/example/konal/web/controller/KonalWebController.java
+++ jivescript-korean-examples-konal/src/main/java/com/socurites/jive/example/konal/web/controller/KonalWebController.java
@@ -2,19 +2,16 @@ package com.socurites.jive.example.konal.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import com.socurites.jive.core.bot.JiveScriptBot;
 import com.socurites.jive.core.bot.builder.JiveScriptReplyBuilder;
+import com.socurites.jive.example.konal.web.domain.KonalDistrict;
+import com.socurites.jive.example.konal.web.repository.KonalDistrictRepository;
 import com.socurites.jive.ext.analyze.entity.JiveExtDomainEntity;
 import com.socurites.jive.ext.analyze.entity.JiveScriptExtBot;
 import com.socurites.jive.ext.analyze.entity.JiveScriptExtReplyBuilder;
@@ -23,6 +20,9 @@ import com.socurites.jive.ext.analyze.entity.JiveScriptExtReplyBuilder;
 public class KonalWebController {
 	@Autowired
 	private JiveScriptBot bot;
+	
+	@Autowired
+	private KonalDistrictRepository disctrictRepository;
 	
 	@Autowired
 	private RestTemplate restTemplate;
@@ -45,11 +45,15 @@ public class KonalWebController {
 			System.out.println(extDomain);
 			
 			try {
-				String sido = extDomain.getProp("sido");
-				String sigungu = extDomain.getProp("sigungu");
-				String district = extDomain.getProp("district");
+				String sido = (extDomain.hasProp("sido") ) ? extDomain.getProp("sido") : "";
+				String sigungu = (extDomain.hasProp("sigungu") ) ? extDomain.getProp("sigungu") : "";
+				String town = (extDomain.hasProp("town") ) ? extDomain.getProp("town") : "";
+				
+				KonalDistrict standardDistrcit = disctrictRepository.getStandardDistrcit(sido, sigungu, town);
+				System.out.println("standardDistrcit=" + standardDistrcit);
 
-				ResponseEntity<String>  responseEntity = restTemplate.exchange("http://apis.skplanetx.com/weather/current/hourly?lon=&village=" + district + "&county=" + sigungu + "&lat=&city=" + sido + "&version=1", HttpMethod.GET, httpEntity, String.class);
+				/**
+				ResponseEntity<String>  responseEntity = restTemplate.exchange("http://apis.skplanetx.com/weather/current/hourly?lon=&village=" + standardDistrcit.getTown() + "&county=" + standardDistrcit.getSigungu() + "&lat=&city=" + standardDistrcit.getSido() + "&version=1", HttpMethod.GET, httpEntity, String.class);
 				String responseBody = responseEntity.getBody();
 				
 				JsonObject jsonObj = new JsonParser().parse(responseBody).getAsJsonObject();
@@ -88,6 +92,7 @@ public class KonalWebController {
 				
 				weatherResult += "\n";
 				weatherResult += sky + ", 현재 온도는 " + tempCurr + "(최저: " + tempMin + ", 최고: " + tempMax + "), 습도는 " + humidity + "%입니다.";
+				**/
 			} catch ( Exception e) {
 				e.printStackTrace();
 				return reply.getReplyAsText();
