455fa17c8c8ebcd4a97f6cd180902725aeac9954
naaras.malsami <naras.malsami@gmail.com>
2016-09-30 12:48:00 +0900

    ...

--- jivescript-korean-examples-konal/src/main/java/com/socurites/jive/example/konal/web/repository/KonalDistrictRepository.java
+++ jivescript-korean-examples-konal/src/main/java/com/socurites/jive/example/konal/web/repository/KonalDistrictRepository.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Repository;
 
 import com.socurites.jive.example.konal.web.domain.KonalDistrict;
